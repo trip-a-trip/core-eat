81925fc15b9a44dc1c2be361d11ada4b7bc709ea
Igor Kamyshev <igor@kamyshev.me>
2020-12-28 13:25:18 +0700

    feat(venue): add get venue endpoint

--- src/eat/presentation/http/venue.controller.ts
+++ src/eat/presentation/http/venue.controller.ts
@@ -1,4 +1,9 @@
-import { ApiOkResponse, ApiNotFoundResponse, ApiQuery } from '@nestjs/swagger';
+import {
+  ApiOkResponse,
+  ApiNotFoundResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import {
   Controller,
   Get,
@@ -6,6 +11,7 @@ import {
   Query,
   UseInterceptors,
   ParseArrayPipe,
+  Param,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -17,6 +23,7 @@ import { Venue } from '&app/eat/domain/venue.entity';
 import { TransformInterceptor, ParseFloatPipe } from '&app/lib/nest';
 
 @Controller('/v1/venue')
+@ApiTags('venue')
 @UseInterceptors(TransformInterceptor)
 export class VenueController {
   constructor(
@@ -34,11 +41,20 @@ export class VenueController {
     return venues;
   }
 
+  @Get('/:id')
+  @ApiOkResponse({ type: Venue })
+  @ApiNotFoundResponse()
+  async get(@Param('id') id: string) {
+    const venue = await this.repo.findOneOrFail(id);
+
+    return venue;
+  }
+
   @Get('/')
   @ApiOkResponse({ type: Venue })
   @ApiQuery({ name: 'skipIds', type: 'string', isArray: true, required: false })
   @ApiNotFoundResponse()
-  async get(
+  async choice(
     @Query('userId') userId: string,
     @Query('latitude', ParseFloatPipe) latitude: number,
     @Query('longitude', ParseFloatPipe) longitude: number,
